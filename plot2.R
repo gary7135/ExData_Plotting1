40eb16b2e218802b4b32a4c8cba9fc15fc647583
Gary Mu <gary7135@gmail.com>
2017-11-09 22:39:54 -0800

    Code that produces 4 plots. One .R and .Rmd files.

---
+++ plot2.R
@@ -0,0 +1,30 @@
+require(tidyverse)
+
+d <- read.csv(file = 'household_power_consumption.txt', sep = ';')
+
+#filter only dates that are between 2007-02-01 and 2007-02-02
+d <- d %>% filter(Date %in% c('1/2/2007', '2/2/2007'))
+
+d[d =='?'] <- NA
+
+#Transform column 3 to 9 into numeric.
+d[,3:9] <- apply(d[,3:9], 2, as.numeric)
+
+#now transform time series data into appropriate forms
+d$date2 <- as.Date(d$Date, format = '%d/%m/%Y')
+date_time <- paste0(as.character(d$Date), ' ', as.character(d$Time))
+d$datetime <- strptime(date_time, '%d/%m/%Y %H:%M:%S')
+
+plot(x = d$datetime, 
+     y = d$Global_active_power, 
+     type = 'l',
+     xlab = '',
+     ylab = 'Global Active Power (kilowatts)')
+
+
+dev.copy(png, 'plot2.png', width = 800, height = 600)
+dev.off()
+
+
+
+
---
+++ plot4.R
@@ -0,0 +1,66 @@
+require(tidyverse)
+
+d <- read.csv(file = 'household_power_consumption.txt', sep = ';')
+
+#filter only dates that are between 2007-02-01 and 2007-02-02
+d <- d %>% filter(Date %in% c('1/2/2007', '2/2/2007'))
+
+d[d =='?'] <- NA
+
+#Transform column 3 to 9 into numeric.
+d[,3:9] <- apply(d[,3:9], 2, as.numeric)
+
+#now transform time series data into appropriate forms
+d$date2 <- as.Date(d$Date, format = '%d/%m/%Y')
+date_time <- paste0(as.character(d$Date), ' ', as.character(d$Time))
+d$datetime <- strptime(date_time, '%d/%m/%Y %H:%M:%S')
+
+par(mfrow = c(2,2))
+
+#plot 1.1
+plot(x = d$datetime, 
+     y = d$Global_active_power, 
+     type = 'l',
+     xlab = '',
+     ylab = 'Global Active Power (kilowatts)')
+
+#plot 1.2
+plot(x = d$datetime, 
+     y = d$Voltage, 
+     type = 'l',
+     xlab = '',
+     ylab = 'Voltage')
+
+#plot 2.1
+plot(x = d$datetime, 
+     y = d$Sub_metering_1, 
+     type = 'l',
+     xlab = '',
+     ylab = 'Energy sub meeting',
+     col = 'black')
+par(new = T)
+lines(x = d$datetime, 
+      y = d$Sub_metering_2, 
+      col = 'red')
+par(new = T)
+lines(x = d$datetime, 
+      y = d$Sub_metering_3, 
+      col = 'blue')
+
+legend( 'topright', 
+        legend = c('Sub_meeting_1','Sub_meeting_2', 'Sub_meeting_3'),
+        col = c('black', 'red', 'blue'),
+        lty = 1, 
+        cex = .5)
+par(new = F)
+
+
+#plot 2.2
+plot(x = d$datetime, 
+     y = d$Global_reactive_power, 
+     type = 'l',
+     xlab = '',
+     ylab = 'Global_reactive_power')
+
+dev.copy(png, 'plot4.png', width = 800, height = 600)
+dev.off()
